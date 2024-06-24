d335450e2502aff21b42b0afe392dafc518f463c
Linus J <30862662+linus-jansson@users.noreply.github.com>
2024-06-24 12:05:20 +0200

    chore: cleanup

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import * as fs from 'fs';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000';
 
 type ResponseData = {letters: number[]} | { error: string };
@@ -22,17 +25,17 @@ const days_elapsed_since_then = () => {
     return Math.floor(diff / ONE_DAY_IN_MILISECONDS);
 }
 
-// const MAGIC_NUMBER = 904 + days_elapsed_since_then();
-const MAGIC_NUMBER = 906;
+const MAGIC_NUMBER = 904 + days_elapsed_since_then() - 2;
 
 function craftFetchRequest(n: number, word: string, someId: number = 904) {
     return fetch(`${API_BASE_URL}?n=${n}&guess=${word}&id=${someId}`)
 }
 
-enum Letter {
-    NotPresent = -1,
-    WrongPosition = 0,
-    CorrectPosition = 1
+
+enum LETTER {
+    WRONG = -1,
+    KINDA = 0,
+    CORRECT = 1
 }
 
 
@@ -46,34 +49,38 @@ enum Letter {
         - If the number is 1, then the char is present in the correct position
             The char is in the correct position, so do nothing.
 */
-const filterFunction = (response_array: Letter[], candidate: string, word: string) => {
-    const tested_chars = new Set<string>();
+const filterFunction = (response_array: LETTER[], candidate: string, word: string) => {
     for (let index = 0; index < response_array.length; index++) {
         const current_status = response_array[index];
         const current_char_in_word = word[index];
         const current_char_in_candidate = candidate[index];
-        tested_chars.add(current_char_in_candidate);
-
+        
         switch (current_status) {
-            case Letter.NotPresent:
+            case LETTER.WRONG: // Character not present in the answer
                 if (candidate.includes(current_char_in_word)) {
-                    console.log(`Filtering out ${candidate} because it contains ${current_char_in_word} which is not present.`);
+                    // console.log(`Filtering out ${candidate} because it contains ${current_char_in_word} which is not present.`);
                     return false;
                 }
                 break;
-            case Letter.WrongPosition:
+            case LETTER.KINDA:
+                // Check if the character is at the indexed position or if it is present only once
+                const char_count_in_word = word.split("").filter(char => char === current_char_in_word).length;
+                if (char_count_in_word > 1)
+                    break;
+
                 if (current_char_in_candidate === current_char_in_word) {
-                    console.log(`Filtering out ${candidate} because ${current_char_in_word} is in the wrong position.`);
+                    // console.log(`Filtering out ${candidate} because ${current_char_in_word} is in the wrong position or present only once.`);
                     return false;
                 }
                 break;
-            case Letter.CorrectPosition:
+            case LETTER.CORRECT: // Character in the correct position
                 if (current_char_in_candidate !== current_char_in_word) {
-                    console.log(`Filtering out ${candidate} because ${current_char_in_word} should be in position ${index}.`);
+                    // console.log(`Filtering out ${candidate} because ${current_char_in_word} should be in position ${index}.`);
                     return false;
                 }
                 break;
             default:
+                // Should never reach here
                 throw new Error("Invalid status");
         }
     }
@@ -84,8 +91,7 @@ const filterFunction = (response_array: Letter[], candidate: string, word: strin
 /*
     loop through list of words
     for each word, send a requst to the API
-    
-
+    parse the response and filter the list of words based on response
     repeat until response array is all 1's
 */
 async function moreCalculatedGuess(words: string[]): Promise<{ correctWord: string, tries: number }> {
@@ -100,12 +106,13 @@ async function moreCalculatedGuess(words: string[]): Promise<{ correctWord: stri
         console.log(`Trying word: ${current_word}, Remaining words: ${currentWordList.length}, Response: ${letters}`);
 
         if (letters === null) { // If the word is invalid, remove it and continue
+            console.log(`Invalid word: ${current_word}`)
             currentWordList = currentWordList.filter(candidate => candidate !== current_word);
             tries++;
             continue;
         }
 
-        if (letters.every(status => status === 1)) { // All letters are correct
+        if (letters.every(status => status === LETTER.CORRECT)) { // All letters are correct
             return { correctWord: current_word, tries: tries + 1 }; // Return the correct word and number of tries
         } else {
             // Filter the list based on the response
@@ -121,14 +128,43 @@ async function moreCalculatedGuess(words: string[]): Promise<{ correctWord: stri
 
 
 (async() => {
-    console.log("MAGIC_NUMBER", MAGIC_NUMBER);
+    console.log("Magic John Screenprotector", MAGIC_NUMBER);
     let wordsList = JSON.parse(fs.readFileSync('words.json', 'utf8')) as string[];
-    // scramble wordslist
     const {correctWord, tries} = await moreCalculatedGuess(wordsList);
-    if (correctWord.length === 0) {
-        console.log("Word not found");
-        console.log("Tries: ", tries);
-        return;
-    }
-    console.log("word ", `"${correctWord}"`, " was found in", tries, "tries");
+    console.log(`Word ${correctWord} ${(correctWord.length === 0) ? "was not found": "was found"} in ${tries} tries`);
 })();
+
+
+/*
+
+{
+  "guesses": [
+    {
+      "word": "testa",
+      "result": [ -1, 0, -1, -1, -1]
+    },
+    {
+      "word": "degel",
+      "result": [-1, -1, 1, 1, -1]
+    },
+    {
+      "word": "pågen",
+      "result": [ -1, -1, 1, 1, 1]
+    },
+    {
+      "word": "ingen",
+      "result": [-1, 1, 1, 1, 1]
+    },
+    {
+      "word": "ängen",
+      "result": [ -1, 1, 1, 1, 1]
+    },
+    {
+      "word": "ungen",
+      "result": [1, 1, 1, 1, 1]
+    }
+  ],
+  "currentGuess": "",
+  "gameId": 906
+}
+*/
