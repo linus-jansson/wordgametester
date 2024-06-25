1f6102dd6ac24c980c0628d737a5d1a35c3f147f
Linus Jansson <linus.jansson2@uppsala.se>
2024-06-25 22:58:44 +0200

    debbugging

--- wordguesser/main.ts
+++ wordguesser/main.ts
@@ -14,8 +14,9 @@ const main = async () => {
     const wordsList = Object.freeze(JSON.parse(fs.readFileSync('../words/valid-words.json', {encoding: "utf8"}))) as string[];
     const {correctWord, tries} = await findCorrectWord(wordsList, options);
     // Some charachter in this makes webhook think its not utf8
+    const wasWasNotFound = (correctWord.length === 0) ? "was not found": "was found";
     const correctWordText = `Word ${correctWord} ${(correctWord.length === 0) ? "was not found": "was found"} in ${tries} tries`;
-    const correctWordText2 = `${correctWord} ${tries}`
+    const correctWordText2 = `${correctWord} ${wasWasNotFound} ${tries}`
     const goingToWrite = Buffer.from(correctWordText2, 'utf8').toString();
     fs.writeFileSync('output.txt', goingToWrite);
     console.log(`Word "${correctWord}" ${(correctWord.length === 0) ? "was not found": "was found"} in ${tries} tries`);
