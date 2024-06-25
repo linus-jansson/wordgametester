6d06918928a79edea55995360153969b1384891a
Linus Jansson <linus.jansson2@uppsala.se>
2024-06-25 19:32:12 +0200

    feat: parallelism on top

--- wordfetcher/wordfetcher.spec.ts
+++ wordfetcher/wordfetcher.spec.ts
@@ -1,89 +1,92 @@
-import { test, expect } from '@playwright/test';
-import fs from 'fs';
+import { test } from '@playwright/test';
+import fs from 'fs/promises';
 
-test('get all words', async ({ page }) => {
+const CONQURENT_PAGES = 30;
+const BASE_URL = 'https://www.kryssakuten.se';
+const PAGE_URL = BASE_URL + '/ord-som-inneh%C3%A5ller/%3F%3F%3F%3F%3F?minletters=5&maxletters=5';
+
+test('get all words parallel', async ({ browser }) => {
     test.setTimeout(0);
-    page.setViewportSize({ width: 1080, height: 720 });
-    
-    const baseUrl = 'https://www.kryssakuten.se';
-    const pageUrl = baseUrl + '/ord-som-inneh%C3%A5ller/%3F%3F%3F%3F%3F?minletters=5&maxletters=5'
-    const collectedWords:any = [];
-    let isLastPage = false;
-    await page.goto(pageUrl);
-    const cookie_accept = await page.getByText("Jag samtycker");
+    const collectedWords: string[] = [];
+    const invalidWords: string[] = [];
+    const pageUrls: string[] = [];
+    const page = await browser.newPage();
+    await page.setViewportSize({ width: 1080, height: 720 });
+    await page.goto(PAGE_URL);
+    const cookie_accept = page.getByText("Jag samtycker");
     await cookie_accept.click();
 
     const allPageNumberElements = await page.locator('.pagenumber').all();
-    const lastRealPageNumber = await allPageNumberElements[allPageNumberElements.length - 2].innerText();
-    
-    while (!isLastPage) {
-        const pagnition_next = await page.getByText("Nästa").first();
-        const pagnition_next_href = await pagnition_next.getAttribute('href');
-        const parsedNextUrl = new URL(pagnition_next_href!, baseUrl);
-        // get query params
-        const searchParams = parsedNextUrl.searchParams;
-        const nextClickPageNumber = searchParams.get('pageno')!;
-
-        // collect all links which href includes "/ord/"
-        const links = await page.$$('a');
-
-        for (const link of links) {
-            const href = await link.getAttribute('href');
-            if (!href || !href.includes("/ord/")) {
-                continue;
-            }
-
-            const word = href.split("/").pop();
-
-            if (!word)
-            {
-                console.log("Skipping word -", href, word)
-                continue;
-            }
-            
-            if (word.length < 5 || word.length > 5)
-            {
-                console.log("Skipping word -", href, word)
-                continue;
-            }
-
-
-            // if word contains any non-alphabetic characters, skip it
-            if (word.match(/[^a-zåäö]/i)) {
-                console.log("Skipping word -", href, word)
-                continue;
-            }
-
-            // check if word is url encoded or not and decode it
-            if (word.includes("%")) {
-                const decodedWord = decodeURIComponent(word);
-                collectedWords.push(decodedWord);
-            } else {
-                collectedWords.push(word);
-            }
-
-            console.log("Adding new word -", href, collectedWords[collectedWords.length - 1])
-        }
-
-        // if currentpage is the last page, break the loop
-        const get_current_page = page.locator('.pagenumbercurrent').first();
-        const current_page = await get_current_page.innerText();
-        console.log("Current page", current_page, "Next page", nextClickPageNumber, "max page", lastRealPageNumber, "collected words", collectedWords.length)
-
-        if (current_page === nextClickPageNumber) {
-            isLastPage = true;
-            break;
-        }
-
-        await page.goto(baseUrl + pagnition_next_href);
-        // repeat the same process for the next page
+    const lastRealPageNumber = parseInt(await allPageNumberElements[allPageNumberElements.length - 2].innerText(), 10);
+    console.log("Last real page number:", lastRealPageNumber);
+    // Collect all page URLs
+    for (let i = 1; i <= lastRealPageNumber; i++) {
+        const url = `${PAGE_URL}&pageno=${i}`;
+        pageUrls.push(url);
     }
 
-    // filter out duplicates
-    const uniqueWords = [...new Set(collectedWords)];
-    
-    fs.writeFileSync('words.json', JSON.stringify(uniqueWords));
+    await page.close();
 
-    // Expect a title "to contain" a substring.
-    await expect(collectedWords.length).toBeCloseTo(14505);
+    const scrapePage = async (url: string): Promise<void> => {
+        const page = await browser.newPage();
+
+        try {
+            await page.goto(url);
+
+            const links = await page.$$('.search-result-word-box>a:first-child');
+            const hrefs = await Promise.all(links.map(async (link) => await link.getAttribute('href')));
+
+            for (const link of hrefs) {
+                if (!link || !link.includes("/ord/")) {
+                    continue;
+                }
+
+                let word = link.split("/").pop();
+
+                if (!word) {
+                    continue;
+                }
+
+                if (word.includes("%")) {
+                    word = decodeURIComponent(word);
+                }
+
+                if (word.length !== 5) {
+                    continue;
+                }
+
+                if (word.match(/[^a-zåäö]/i)) {
+                    if (!invalidWords.includes(word)) {
+                        invalidWords.push(word);
+                    }
+                } else {
+                    if (!collectedWords.includes(word)) {
+                        collectedWords.push(word);
+                    }
+                }
+            }
+
+            // Print the accumulated words immediately
+            console.log(`Scraped from ${url}\n Accumulated valid words: ${collectedWords.length}\n Accumulated invalid words: ${invalidWords.length}\n Total accumulated words: ${collectedWords.length + invalidWords.length}`);
+        } finally {
+            await page.close();
+        }
+    };
+
+    const { default: pLimit } = await import('p-limit');
+    const limit = pLimit(CONQURENT_PAGES);
+
+    // Scrape all pages with controlled concurrency
+    const scrapePromises = pageUrls.map(url => limit(() => scrapePage(url)));
+    await Promise.all(scrapePromises);
+
+    // Remove duplicates and write to files
+    const uniqueWords = [...new Set(structuredClone(collectedWords).sort())];
+    await fs.writeFile('../words/valid-words.json', JSON.stringify(uniqueWords), { encoding: 'utf-8' });
+
+    const uniqueInvalidWords =  [...new Set(structuredClone(invalidWords).sort())];
+    await fs.writeFile('../words/invalid-words.json', JSON.stringify(uniqueInvalidWords), { encoding: 'utf-8' });
+
+    const uniqueAllWords =  [...new Set(uniqueWords.concat(uniqueInvalidWords).sort())];
+    await fs.writeFile('../words/all-words.json', JSON.stringify(uniqueAllWords), { encoding: 'utf-8' });
 });
